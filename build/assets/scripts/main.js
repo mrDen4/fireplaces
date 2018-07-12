30734f868dda07526bae6175dd8c35ae93096e9d
Denis Davydov <den.saint.k@gmail.com>
2018-07-13 01:47:16 +0300

    add 2 pages with styles but wothout adaptive

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -4,4 +4,11 @@ $(document).ready(() => {
         $('.header__list').toggleClass('header__list--active');
         console.log('click');
     });
+
+    $('.another__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: $('.another__btn-left'),
+        nextArrow: $('.another__btn-right')
+    });
 });

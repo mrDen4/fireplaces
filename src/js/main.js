270963f056515c1229964044e65129244c8af882
Denis Davydov <den.saint.k@gmail.com>
2018-08-07 12:33:30 +0300

    add popup

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,16 @@ $(document).ready(() => {
     $('.header__btn').on('click', function() {
         $('.header__contacts').toggleClass('header__contacts--active');
         $('.header__list').toggleClass('header__list--active');
-        console.log('click');
+    });
+
+    $('.buy__btn').on('click', function() {
+        $('.product__modal-bg').toggleClass('product__modal-bg--active');
+        $('.product__modal').toggleClass('product__modal--active');
+    });
+
+    $('.product__modal-bg').on('click', function() {
+        $('.product__modal-bg').toggleClass('product__modal-bg--active');
+        $('.product__modal').toggleClass('product__modal--active');
     });
 
     $('.another__slider').slick({
